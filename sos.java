78339e3fc2628497327ea06406c3a6e3921b535d
Ifedi <ifediohiaeri@gmail.com>
2023-03-04 16:09:46 +0000

    resolved

--- sos.java
+++ sos.java
@@ -3,6 +3,7 @@ We pray thee oh Lord
 Save Nigeria
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 May the wicked not succeed
 In Jesus'mighty name
 Amen!
@@ -16,3 +17,6 @@ Amen
 Praise the Lord
 Alleluyah!
 >>>>>>> 455f69f (Added Alleluyah)
+=======
+Amen
+>>>>>>> 6af8055 (Revert "Added Alleluyah")
