46f86436e073be4ef1fd9742ffea3474ca0866d8
Ifedi <ifediohiaeri@gmail.com>
2023-03-04 15:07:20 +0000

    conflict resolved

--- sos.java
+++ sos.java
@@ -1,6 +1,12 @@
 Save our soul
 We pray thee oh Lord
 Save Nigeria
+<<<<<<< HEAD
 May the wicked not succeed
 In Jesus'mighty name
 Amen!
+=======
+Amen
+Praise the Lord
+Alleluyah!
+>>>>>>> 455f69f (Added Alleluyah)
